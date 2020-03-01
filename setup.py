e2b1cd051371f4d2f1ac63b00d065ac90f468f23
Ben Hayes <ben@benhayes.net>
2020-03-01 14:13:50 +0000

    Setup package structure

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(name='beat-tracking-tcn', version='1.0', packages=find_packages())
+setup(name='beat_tracking_tcn', version='1.0', packages=find_packages())
