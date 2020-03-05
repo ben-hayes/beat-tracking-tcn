c9a125a05d77d771533495ab5609bb489587d423
Ben Hayes <ben@benhayes.net>
2020-03-05 20:07:00 +0000

    Implement k-fold cross validation

---
+++ scripts/k_fold_cross_validation.py
@@ -0,0 +1,136 @@
+"""
+Ben Hayes 2020
+
+ECS7006P Music Informatics
+
+Coursework 1: Beat Tracking
+
+File: scripts/k_fold_cross_validation.py
+
+Descrption: Train several BeatNet models on different folds of the dataset
+            and compare their performance.
+"""
+from argparse import ArgumentParser
+import math
+import os
+
+from torch import device
+from torch.utils.data import random_split, ConcatDataset
+
+from beat_tracking_tcn.models.beat_net import BeatNet
+from train import test_model,\
+                   train_loop,\
+                   save_model,\
+                   make_data_loaders,\
+                   split_dataset,\
+                   load_dataset
+
+
+def parse_args():
+    parser = ArgumentParser(
+        description="Perform k-fold cross-validation on a BeatNet model")
+
+    parser.add_argument("spectrogram_dir", type=str)
+    parser.add_argument("label_dir", type=str)
+    parser.add_argument(
+        "-k",
+        "--num_folds",
+        default=8,
+        type=str,
+        help="Number of folds for cross-validation")
+    parser.add_argument(
+        "-o",
+        "--output_file",
+        default=None,
+        type=str,
+        help="Where to save trained model.")
+    parser.add_argument(
+        "-n",
+        "--num_epochs",
+        default=100,
+        type=int,
+        help="Number of training epochs")
+    parser.add_argument(
+        "-s",
+        "--davies_stopping_condition",
+        action="store_true",
+        help="Use Davies & Bock's stopping condition " +
+             "(ignores number of epochs)")
+    parser.add_argument(
+        "-b",
+        "--batch_size",
+        type=int,
+        default=1,
+        help="Batch size to use for training.")
+    parser.add_argument(
+        "-c",
+        "--cuda_device",
+        type=int,
+        default=None,
+        help="CUDA device index for training. CPU used if none specified.")
+
+    return parser.parse_args()
+
+
+def make_fold_datasets(dataset, num_folds):
+    dataset_length = len(dataset)
+    fold_size = math.floor(dataset_length / num_folds)
+    remainder = dataset_length - (num_folds * fold_size)
+
+    fold_counts = [fold_size] * num_folds
+    for i in range(remainder):
+        fold_counts[i] += 1
+
+    fold_sets = random_split(dataset, fold_counts)
+
+    return fold_sets
+
+def iterate_folds(fold_sets):
+    num_sets = len(fold_sets)
+    for i in range(num_sets):
+        test_set = fold_sets[i]
+        val_set = fold_sets[(i + 1) % num_sets]
+        train_sets = fold_sets[:i] + fold_sets[(i + 2) % num_sets:]
+        train_set = ConcatDataset(train_sets)
+
+        yield train_set, val_set, test_set
+
+def make_fold_output_name(base_name, fold):
+    filename, ext = os.path.splitext(base_name)
+    new_name = "%s.fold%.3d%s" % (filename, fold, ext)
+    return new_name
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    dataset = load_dataset(args.spectrogram_dir, args.label_dir)
+    fold_sets = make_fold_datasets(dataset, args.num_folds)
+
+    cuda_device = device('cuda:%d' % args.cuda_device)\
+                  if args.cuda_device is not None else None
+
+    for k, datasets in enumerate(iterate_folds(fold_sets)):
+        train, val, test = datasets
+        model = BeatNet()
+        if cuda_device is not None:
+            model.cuda(args.cuda_device)
+
+        output_file = make_fold_output_name(args.output_file, k)
+
+        train_loader, val_loader, test_loader = make_data_loaders(
+            (train, val, test),
+            batch_size=args.batch_size)
+
+        train_loop(
+            model,
+            train_loader,
+            val_loader=val_loader,
+            num_epochs=args.num_epochs,
+            cuda_device=cuda_device,
+            output_file=args.output_file,
+            davies_stopping_condition=args.davies_stopping_condition)
+
+        if args.output_file is not None:
+            save_model(model, args.output_file)
+
+        test_model(model, test_loader, cuda_device=cuda_device)
