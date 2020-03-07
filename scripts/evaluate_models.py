92ce04cc77a5ce589a7e4b3ab6bf56ee3aa693c6
Ben Hayes <ben@benhayes.net>
2020-03-07 18:34:54 +0000

    Begin evaluation code

---
+++ scripts/evaluate_models.py
@@ -0,0 +1,61 @@
+"""
+Ben Hayes 2020
+
+ECS7006P Music Informatics
+
+Coursework 1: Beat Tracking
+
+File: scripts/evaluate_models.py
+
+Descrption: Evaluate the given beat tracking models according to metrics as
+            outlined in Davies et al 2009 [1]
+
+References:
+    [1] M. E. P. Davies, N. Degara, and M. D. Plumbley, ‘Evaluation Methods for
+        Musical Audio Beat Tracking Algorithms’, p. 17.
+"""
+from argparse import ArgumentParser
+import pickle
+
+from torch.utils.data import DataLoader
+import torch.nn as nn
+
+from beat_tracking_tcn.models.beat_net import BeatNet
+from beat_tracking_tcn.datasets.ballroom_dataset import BallroomDataset
+from beat_tracking_tcn.utils.training import forward_batch
+
+
+def parse_args():
+    parser = ArgumentParser(
+        description="Perform evaluation on given BeatNet models")
+
+    parser.add_argument("-d", "--saved_k_fold_dataset", type=str)
+    parser.add_argument("model_checkpoints", type=str, nargs='+')
+
+    return parser.parse_args()
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    ds_root, ext = os.path.splitext(args.saved_k_fold_dataset)
+    ds_root = os.path.splitext(ds_root)[0]
+
+    criterion = nn.BCELoss()
+
+    for k, model_checkpoint in enumerate(args.model_checkpoints):
+        dataset_file = "%s.%.3d%s" % (ds_root, k, ext)
+        with open(dataset_file, 'rb') as f:
+            _, _, test = pickle.load(f)
+
+        with open(model_checkpoint, 'rb') as f:
+            state_dict = pickle.load(f)
+
+        test_loader = DataLoader(test, batch_size=1, num_workers=8)
+
+        model = BeatNet()
+        model.load_state_dict(state_dict)
+        model.eval()
+
+        for i, batch in enumerate(test_loader):
+            outputs, loss = forward_batch(model, criterion, batch)
+            print(loss)
