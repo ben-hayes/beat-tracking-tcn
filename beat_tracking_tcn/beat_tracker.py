3c3fc0a7ab9b11127359a16c7536c74d71455fde
Ben Hayes <ben@benhayes.net>
2020-03-08 12:17:53 +0000

    Add downbeat functionality to beat_tracker call

--- beat_tracking_tcn/beat_tracker.py
+++ beat_tracking_tcn/beat_tracker.py
@@ -25,10 +25,15 @@ SR = 22050
 DEFAULT_CHECKPOINT_PATH = os.path.join(
         os.path.dirname(__file__),
         'checkpoints/default_checkpoint.torch')
+DEFAULT_DOWNBEAT_CHECKPOINT_PATH = os.path.join(
+        os.path.dirname(__file__),
+        'checkpoints/default_downbeat_checkpoint.torch')
 
 
 model = BeatNet()
 model.eval()
+downbeat_model = BeatNet(downbeats=True)
+downbeat_model.eval()
 
 dbn = DBNBeatTrackingProcessor(
     min_bpm=55,
@@ -36,13 +41,24 @@ dbn = DBNBeatTrackingProcessor(
     transition_lambda=100,
     fps=(SR / HOP_LENGTH_IN_SAMPLES),
     online=True)
+downbeat_dbn = DBNBeatTrackingProcessor(
+    min_bpm=10,
+    max_bpm=75,
+    transition_lambda=100,
+    fps=(SR / HOP_LENGTH_IN_SAMPLES),
+    online=True)
 
 
-def beat_activations_from_spectrogram(spectrogram, checkpoint_file=None):
+def beat_activations_from_spectrogram(spectrogram, checkpoint_file=None, downbeats=True):
     if checkpoint_file is not None:
-        load_checkpoint(model, checkpoint_file)
+        load_checkpoint(
+            downbeat_model if downbeats else model,
+            checkpoint_file)
     else:
-        load_checkpoint(model, DEFAULT_CHECKPOINT_PATH)
+        load_checkpoint(
+            downbeat_model if downbeats else model,
+            DEFAULT_DOWNBEAT_CHECKPOINT_PATH
+                if downbeats else DEFAULT_CHECKPOINT_PATH)
 
     with torch.no_grad():
         if type(spectrogram) is not torch.Tensor:
@@ -53,17 +69,39 @@ def beat_activations_from_spectrogram(spectrogram, checkpoint_file=None):
         else:
             spectrogram_tensor = spectrogram
         
-        return model(spectrogram_tensor).numpy()
+        return downbeat_model(spectrogram_tensor).numpy() if downbeats\
+               else model(spectrogram_tensor).numpy()
 
-def predict_beats_from_spectrogram(spectrogram, checkpoint_file=None):
-    beat_activations =\
-        beat_activations_from_spectrogram(spectrogram, checkpoint_file)
-    dbn.reset()
-    predicted_beats = dbn.process_offline(beat_activations.squeeze())
-    return predicted_beats
+def predict_beats_from_spectrogram(
+        spectrogram,
+        checkpoint_file=None,
+        downbeats=True):
+
+    raw_activations =\
+        beat_activations_from_spectrogram(
+            spectrogram,
+            checkpoint_file,
+            downbeats).squeeze()
+    if downbeats:
+        beat_activations = raw_activations[0]
+        downbeat_activations = raw_activations[1]
+
+        dbn.reset()
+        predicted_beats = dbn.process_offline(beat_activations.squeeze())
+        downbeat_dbn.reset()
+        predicted_downbeats =\
+            downbeat_dbn.process_offline(downbeat_activations.squeeze())
+
+        return predicted_beats, predicted_downbeats
+    else:
+        beat_activations = raw_activations
+        dbn.reset()
+        predicted_beats = dbn.process_offline(beat_activations.squeeze())
+        return predicted_beats
 
 
-def beatTracker(input_file, checkpoint_file=None):
+
+def beatTracker(input_file, checkpoint_file=None, downbeats=True):
     mag_spectrogram = trim_spectrogram(
         create_spectrogram(
             input_file,
@@ -72,4 +110,7 @@ def beatTracker(input_file, checkpoint_file=None):
             N_MELS),
         TRIM_SIZE).T
     
-    return predict_beats_from_spectrogram(mag_spectrogram, checkpoint_file)
+    return predict_beats_from_spectrogram(
+        mag_spectrogram,
+        checkpoint_file,
+        downbeats)
